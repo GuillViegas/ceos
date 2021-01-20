dc492737e6af839c10836831d596572a7484961f
GuilViegas <guivfaria@gmail.com>
2021-01-19 22:09:13 -0300

    Encapsulate bitcoin dataset logic

---
+++ bitcoin.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import inspect
+import pandas as pd
+
+
+class BitcoinTimeSeries:
+
+    def __init__(self, dataset=None, file_path=None):
+        if not (dataset or file_path):
+            raise ValueError("BitcoinTimeSeries needs to be inicialized with some dataset")
+        
+        self.dataset = dataset or pd.read_csv(file_path)
+        self.dataset.fillna(method='ffill', inplace=True)
+
+        self.date_from_timestamp()
+
+    def date_from_timestamp(self):
+        if not 'Timestamp' in self.dataset:
+            raise ValueError("Dataset has no timestamp field")
+
+        dates = self.dataset['Timestamp'].apply(datetime.fromtimestamp)
+        
+        self.dataset['Date'] = dates
+        self.dataset.set_index('Date', inplace=True)
+
+        return dates
+
+    def result_set(self, begin_date, end_date, field, indicator, **kwargs):
+        if 'begin_date' in inspect.getargspec(indicator).args:
+            return indicator(self.dataset, begin_date, end_date, field, **kwargs)
+
+        return indicator(self.dataset.loc[begin_date:end_date], field, **kwargs)
